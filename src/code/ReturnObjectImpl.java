fa83e675c1e42ea1e070d6f2bfe89916082a2c66
John Spear <jspears623@gmail.com>
2015-01-26 20:42:11 +0000

    Updated ReturnObjectImpl JDoc code

--- src/code/ReturnObjectImpl.java
+++ src/code/ReturnObjectImpl.java
@@ -1,4 +1,10 @@
 package code;
+/**
+ * A wrapper containing either an object (the result of an operation
+ * on a data structure) or an error value. It contains the a flag to tell whether there has been an error and a details item which will tell you the details of that error
+ *
+ * @author John Spears
+ */
 public class ReturnObjectImpl implements ReturnObject {
 	private boolean error;
 	private ErrorMessage errorDetails; 
@@ -27,15 +33,37 @@ public class ReturnObjectImpl implements ReturnObject {
 	public void setErrorDetails(ErrorMessage errorDetails) {
 		this.errorDetails = errorDetails;
 	}
-	
+	/**
+	 * Returns whether there has been an error
+	 * @return whether there has been an error
+	 */
 	public boolean hasError() {
 		return this.error;
 	}
-	
+	/**
+	 * Returns the error message. 
+	 * 
+	 * This method must return NO_ERROR if and only if
+	 * {@hasError} returns false.
+	 * 
+	 * @return the error message
+	 */
 	public ErrorMessage getError() {
 		return this.errorDetails;
 	}
-	
+	/**
+	 * Returns the object wrapped in this ReturnObject, i.e. the
+	 * result of the operation if it was successful, or null if
+	 * there has been error.
+	 * 
+	 * Note that the output of this method must be null if {@see
+	 * hasError} returns true, but the opposite is not true: if
+	 * {@see hasError} returns false, this method may or may not
+	 * return null.
+	 * 
+	 * @return the return value from the method or null if there has been an
+	 *         error
+	 */
 	public Object getReturnValue() {
 		if (this.hasError() == false) {
 			return this.item;
--- src/code/SampleableListImpl.java
+++ src/code/SampleableListImpl.java
@@ -1,6 +1,17 @@
 package code;
+/**
+ * A sampleable list can be sampled. It only has one method and extends on LinkedList rather than ArrayList 
+ * 
+ * @author John Spears
+ */
 public class SampleableListImpl extends LinkedList implements SampleableList {
 	//using linked list rather than array list as the array list size issue causes confusion
+	/**
+	 * Returns a list containing the first, third, fifth...
+	 * items of this list, or an empty list if the list is empty. 
+	 * 
+	 * @return a list containing the first, third, fifth... items of this list
+	 */	
 	public SampleableList sample(){
 		SampleableList sampledList = new SampleableListImpl();
 		if(!(this.isEmpty())) {
